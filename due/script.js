35ca7a3fa1ab901afd05f878d46d80bf165c91dc
stefan <stefan.emailacct@gmail.com>
2017-12-03 02:53:31 +0000

    popup on page

--- due/script.js
+++ due/script.js
@@ -9,54 +9,52 @@
   
   //onsubmit
   document.querySelector('.ralogin')
-  .addEventListener('submit', function(e) {
-      e.preventDefault();
+    .addEventListener('submit', function(e) {
+        e.preventDefault();
       
-      if(working) {
-        return;
-      }
-        
-      working = true;
+        if(working) {
+          return;
+        }
+          
+        working = true;
       
       
       raform.classList.add('loading');
       state.innerHTML = 'Authenticating VIPs';
       
       setTimeout(function() {
-        raform.classList.add('ok');
+          raform.classList.add('ok');
+
+          var rauser = document.querySelector('.rainput-username input').value;
+          state.innerHTML = '<div class="propa"><img src="https://sb.monetate.net/img/1/748/1304922.png"><p>' + ramessage(rauser) + '</p></div>';
         
-        var rauser = document.querySelector('.rainput-username input').value;
-        state.innerHTML = '<div class="propa"><img src="https://sb.monetate.net/img/1/748/1304922.png"><p>' + ramessage(rauser) + '</p></div>';
-        
-        function ramessage(rauser) {
-          var ramess = ramess || undefined;
-          if(rauser == '') {
+          function ramessage(rauser) {
+              var ramess = ramess || undefined;
+              if(rauser == '') {
+                
+                ramess = '<span id="rawelcome">Welcome back!</span>' + '<br/>';
+                ramess += ".. a lot has happened since you last logged in.";
+                return ramess;
+              }
+              else {
+                ramess = '<span id="rawelcomeuser">Welcome back <span id="rasuser">' + rauser + "!</span></span><br/><br/><br/>";
+                ramess += ".. a lot has happened since you last logged in.";
+                //document.querySelector('button i.spinner').style.top = '65%';
+                return ramess;
+                
+              }//else
             
-            ramess = '<span id="rawelcome">Welcome back!</span>' + '<br/>';
-            ramess += ".. a lot has happened since you last logged in.";
-            return ramess;
-          }
-          else {
-            ramess = '<span id="rawelcomeuser">Welcome back <span id="rasuser">' + rauser + "!</span></span><br/><br/><br/>";
-            ramess += ".. a lot has happened since you last logged in.";
-            //document.querySelector('button i.spinner').style.top = '65%';
-            return ramess;
-            
-          }//else
-          
-        }//ramessage
+          }//ramessage
         
-        setTimeout(function() {
-          state.innerHTML = "log in";
-          //remove all but ralogin
-          raform.className = "ralogin";
-          working = false;
-          
-        }, 3500);
+          setTimeout(function() {
+              state.innerHTML = "log in";
+              //remove all but ralogin
+              raform.className = "ralogin";
+              working = false;
+          }, 3500);
         
       }, 1500);
-    });
-  
-  
+
+  });//onsubmit
   
 })();
