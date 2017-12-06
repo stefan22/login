b1d0752405371c590f6319d598fe89d888741c0b
Stefano <stefano.lissiaeu@gmail.com>
2017-12-06 22:53:52 +0000

    tooltip with session and chatwindow

--- due/tooltip/tooltipup.js
+++ due/tooltip/tooltipup.js
@@ -25,12 +25,13 @@ externalScripts();
             this.mcssone();
             this.mcsstwo();
             this.tooltip();
+            this.session();
           
         }, //init 
 
         mcssone: function () {
             console.info('%c cssone \u221a', 'background:blue;color:white;');
-            var mainone = ' div#tooltip:after{z-index:100; content:"";position:absolute;width:24px;height:0;border-width:24px;border-style:solid;border-color:transparent #eaeaea transparent transparent;top:-59px;left:272px;transform:rotate(90deg);background:transparent}@keyframes totip{0%{opacity:0;top:-200px}100%{opacity:1;top:80px}}.face:hover{animation:shake .82s cubic-bezier(.36,.07,.19,.97) both;transform:translate3d(0,0,0);backface-visibility:hidden;perspective:1000px}@keyframes shake{10%,90%{transform:translate3d(-1px,0,0)}20%,80%{transform:translate3d(2px,0,0)}30%,50%,70%{transform:translate3d(-4px,0,0)}40%,60%{transform:translate3d(4px,0,0)}}.lowrapper{z-index:100; animation-name:totip;animation-duration:1s;position:absolute;transition:top .5s,opacity .5s}#tooltip{animation:shake .82s cubic-bezier(.36,.07,.19,.97) both;transform:translate3d(0,0,0);backface-visibility:hidden;perspective:1000px}label.tooltip{position:relative;border-bottom:2px dotted #980156;color:#5390c1;cursor:pointer}label.tooltip div{display:block !important; width:325px;font-size:.9em;line-height:1.2em;background-color:#eaeaea;color:#fff;text-align:center;padding:20px;display:none;position:absolute;top:70px;right:160px;;height:370px}label.tooltip input:checked~div{display:none}label.tooltip input{display:none}#tooltip .tootop{z-index:100; left:0;top:0;width:325px;height:60px;display:block;border-left:1px solid #e7e7e7;border-top-left-radius:4px;border-top-right-radius:4px;border-top:1px solid #e7e7e7;border-right:1px solid #e7e7e7;background-color:#fff}.tootop h1{color:#d45c95;font-weight:normal}.tootop h1 i{padding:0 .5em;font-size:1.25em}#tooltip .toobottom{z-index:100; display:block;left:0;top:102px;border-left:1px solid #e7e7e7;border-bottom:1px solid #e7e7e7;border-right:1px solid #e7e7e7;border-bottom-left-radius:4px;border-bottom-right-radius:4px;height:270px}#tooltip .toobottom p{color:#823f8f;font-size:1.15em}.pone{margin:1.75em 0 1.5em;border:1px solid #9a8a8a;padding:1em 0;border-radius:4px;text-align:center}.pone a i{margin-right:1.25em}.pone a{text-decoration:none;color:#823f8f;letter-spacing:.5px;width:95%;margin:2em auto}.toobottom p.pone:nth-child(3){margin-top:0}.toobottom button{background-color:#d45b95;padding:15px 5px;color:#fafafa;width:100%;border:1px solid #b7477d;font-size:1.5em;text-align:center;cursor:pointer}.toobottom button:focus,.toobottom button:active{outline:0} .tooltip {opacity:1 !important;}   ';
+            var mainone = ' div#tooltip:after{content:"";position:absolute;width:26px;height:0;border-width:26px;border-style:solid;border-color:transparent #ffffff transparent transparent;top:-52px;left:295px;transform:rotate(90deg);background:transparent}@keyframes totip{0%{opacity:0;top:-200px}100%{opacity:1;top:80px}}.face:hover{animation:shake .82s cubic-bezier(.36,.07,.19,.97) both;transform:translate3d(0,0,0);backface-visibility:hidden;perspective:1000px}@keyframes shake{10%,90%{transform:translate3d(-1px,0,0)}20%,80%{transform:translate3d(2px,0,0)}30%,50%,70%{transform:translate3d(-4px,0,0)}40%,60%{transform:translate3d(4px,0,0)}}.lowrapper{animation-name:totip;animation-duration:1s;position:absolute;transition:top .5s,opacity .5s;right:180px;top:115px}#tooltip{animation:shake .82s cubic-bezier(.36,.07,.19,.97) both;transform:translate3d(0,0,0);backface-visibility:hidden;perspective:1000px;width:366px!important;background-color:#fff;padding:20px;height:412px}label.tooltip{position:relative;color:#5390c1;cursor:pointer}label.tooltip input:checked~div{display:none}label.tooltip input{display:none}#tooltip .tootop{height:108px!important;display:block;border-left:1px solid #e7e7e7;border-top-left-radius:4px;border-top-right-radius:4px;border-top:1px solid #e7e7e7;border-right:1px solid #e7e7e7;background-color:#fff!important}.tootop h1{font-size:3em; color:#823f90!important;font-weight:bold!important;text-align:center;letter-spacing:1px;border-bottom:1px solid #eaeaea;width:90%;margin:0 auto;padding:.5em 0;line-height:35px}.tootop h1 i{padding:.75em 0 .5em; font-size:1.25em}#tooltip .toobottom{display:block!important;left:0!important;top:102px!important;border-left:1px solid #e7e7e7;border-bottom:1px solid #e7e7e7;border-right:1px solid #e7e7e7;border-bottom-left-radius:4px;border-bottom-right-radius:4px;height:280px!important;margin-top:-15px!important;text-align:center!important;padding:0 20px;font-family:"FranklinGothicRegular",Arial,sans-serif}#tooltip .toobottom p{color:#333!important;font-size:16px!important;font-family:"FranklinGothicRegular",Arial,sans-serif; color:#333;}.pone{margin:1.25em 0 1em;border:1px solid #9a8a8a!important;padding:.5em 0;border-radius:4px;text-align:center;background-color:#823f90!important}p.pnull{color:#333;font-size:16px!important;line-height:22px!important;letter-spacing:.15px}span.logcolor{font-weight:bolder;line-height:3em;text-align:center;letter-spacing:.25px!important}.pone a{text-decoration:none!important;color:#fff!important;letter-spacing:.5px;width:95%;margin:2em auto;font-size:17px!important;font-weight:100;line-height:38px;vertical-align:bottom}}.toobottom p.pone:nth-child(3){margin-top:0}.toobottom button{background-color:#fbfbfb!important;padding:15px 5px;color:#333;width:100%;border:1px solid #eaeaea!important;font-size:18px;line-height:24px;text-align:center;cursor:pointer}.toobottom button:focus,.toobottom button:active{outline:0}img.chatup{max-width:36px!important;margin-right:10px;height:auto; display:inline-block;}   ';
 
             var head = document.getElementsByTagName('head')[0];
 
@@ -46,9 +47,9 @@ externalScripts();
 
         mcsstwo: function () {
             console.info('%c csstwo \u221a', 'background:blue;color:white;');
-            var maintwo = ' .site-body {z-index:-1;} .cycle2-engine {z-index:0;} 
-            .cycle-slide.cycle-sentinel {z-index:0;} .cycle-slide {z-index:0;} ';
-
+              var maintwo = ' .tooltip {opacity:1 !important;}  .tooltip,#tooltip,.tootop, .toobottom {z-index:100 !important;} #header-id, .js-header-navigation.header-navigation,.site-body {z-index:150;} #tooltip { border-top-left-radius:4px;border-top-right-radius:4px;border-bottom-left-radius:4px;border-bottom-right-radius:4px; }  span.logcolor a {color:#333 !important; text-decoration:none !important;} .lowrapper {display:none;} #ovlay { width:100%; height:100%; z-index:9999; opacity:.6;} .lowrapper {display:block;}  ';
+            
+          
             var head = document.getElementsByTagName('head')[0];
             function addcss(css) {
                 var s = document.createElement('style');
@@ -64,10 +65,31 @@ externalScripts();
             console.info('%c tooltip \u221a', 'background:blue;color:white;');
             var tooltip = document.createElement('div');
             tooltip.className = "lowrapper";
-            tooltip.innerHTML = '<label class="tooltip"> <input type="checkbox"> <div id="tooltip"> <div class="tootop"> <h1> <i class="fa fa-hand-peace-o" aria-hidden="true"></i> We Missed You <i class="fa fa-hand-peace-o" aria-hidden="true"></i> </i> </h1> </div><div class="toobottom"> <p class="pnull">If you cant remember your login credentials:</p><p class="pone"> <a href="tel:08000831988"> <i class="fa fa-hand-peace-o"></i> Call us on 0800 111 222 </a> </p><p class="pone"> <a id="chaton" onclick="chaton()"><i class="fa fa-hand-peace-o"></i> Click here to chat with us </a> </p><button id="vainow" type="submit" class="btn-submit"> Im ok for now </button> </div></div></label>';
+           tooltip.innerHTML = '  <label class="tooltip"> <input type="checkbox"> <div id="tooltip"> <div class="tootop"> <h1> We\'ve Missed You </h1> </div><div class="toobottom"> <p class="pnull">If you are unable to remember<br/> your login credentials<br/> Call on <span class="logcolor"><a href="tel:08000831988">0800 111 222</a></span> </p><p class="pone"><img class="chatup" src="https://sb.monetate.net/img/1/748/1312856.png" width="36" height="36"><a onclick="liveagent.startChat("5730M000000IG3i");"> Click here to chat with us </a> </p><button id="vainow" type="submit" class="btn-submit"> I\'m ok for now </button> </div></div></label>   ';
 
             var toolhandler = document.querySelector('.header-login');
             toolhandler.appendChild(tooltip);
+       },
+
+       session: function() {
+
+            if(sessionStorage.isVisisted !== "visited") {
+                sessionStorage.isVisisted = 'visited';
+                setTimeout(function(e) {
+                    if(document.querySelector('.lowrapper')) {
+                        var ba = document.querySelector('.lowrapper');
+                        var ovlay = document.querySelector('div');
+                        ovlay.id = 'ovlay';
+                        
+                        ba.style.display = "block";
+
+                    }
+                    
+
+                },1000);
+            }
+
+
        }
 
 
